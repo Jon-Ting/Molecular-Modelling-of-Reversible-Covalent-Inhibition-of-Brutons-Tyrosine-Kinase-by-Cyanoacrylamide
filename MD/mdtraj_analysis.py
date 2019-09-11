fd750d87f41ef616315400dcb34923e082057664
Jonathan Ting <yikchangjonathan.ting@uq.edu.au>
2019-09-11 11:12:50 +1000

    Organised directories for mtb and pdb files

--- MD/mdtraj_analysis.py
+++ MD/mdtraj_analysis.py
@@ -1,11 +1,12 @@
-"""
+'''
 Python script for MD trajectory data analysis.
 Written by Jonathan Yik Chang Ting (Student ID: 44254124) for UQ BAdSc(Hons) Honours Project 2019.
-Project Title: Molecular Modelling of Reversible Covalent Inhibition of Bruton’s Tyrosine Kinase by Cyanoacrylamides
+Project Title: Molecular Modelling of Reversible Covalent Inhibition of Bruton's Tyrosine Kinase by Cyanoacrylamides
 Free to be distributed, edited and utilized for purposes beneficial to the world. :)
 Though recognition of my effort is appreciated! XD
-"""
+'''
 
+import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -13,56 +14,48 @@ import pandas as pd
 sns.set(context='paper', font_scale=1.5)
 # sns.despine()  # Remove "chartjunk"
 
-SINGLE_PLOT_WIDTH, SINGLE_PLOT_HEIGHT = 6, 4
+MD_PATH = "/home/s4425412/data/Honours/MD"
+SINGLE_PLOT_WIDTH, SINGLE_PLOT_HEIGHT = 9, 7
 SUBPLOTS_WIDTH, SUBPLOTS_HEIGHT = 10, 6
 
 
 if __name__ == "__main__":
-    csv_file = "{0}/QM/Conformational_Analysis/Most_stable/Properties_Correlation.csv".format(DATA_PATH)
-    df = pd.read_csv(csv_file, index_col=0)
-    print(df)
 
-    # Ligand LUMO Energy
-    # sorted_df = df.sort_values(by=["Ligand LUMO Energy (kcal/mol)"])
-    fig = plt.figure(figsize=(SINGLE_PLOT_WIDTH, SINGLE_PLOT_HEIGHT))
-    fig.subplots_adjust(top=0.9, bottom=0.1, left=0.1, right=0.95)
-    ax = sns.scatterplot(x="Ligand LUMO Energy (kcal/mol)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand", legend=False, s=100)
-    for line in range(1, df.shape[0] + 1):
-        ax.text(df["Ligand LUMO Energy (kcal/mol)"][line], df["Addition Barrier (kcal/mol)"][line] + 0.4,
-                df["Ligand"][line], horizontalalignment='center', size='small', color='black')
-    plt.ylim(None, None); plt.xlim(None, None)
-    plt.tight_layout()
-    plt.savefig("Ligand LUMO Energy")
-
-    # Distortion/Interaction Analysis
-    fig = plt.figure(figsize=(SUBPLOTS_WIDTH, SUBPLOTS_HEIGHT))
-    # fig, axes = plt.subplots(nrows=2, ncols=2, sharex=False, sharey=True)
-    fig.subplots_adjust(top=0.9, bottom=0.1, left=0.1, right=0.95, wspace=0.3, hspace=0.45)
-    fig.suptitle("Correlation between Distortion/Interaction Energies and Addition Barrier", horizontalalignment='center', fontsize=16, weight='bold')
-    ax1 = fig.add_subplot(2, 2, 1)
-    p1 = sns.scatterplot(x="Thiolate Distortion Energy (kcal/mol)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand", legend=False, s=100)
-    for line in range(1, df.shape[0] + 1):
-        ax1.text(df["Thiolate Distortion Energy (kcal/mol)"][line]-0.03, df["Addition Barrier (kcal/mol)"][line],
-                df["Ligand"][line], horizontalalignment='right', size='small', color='black')
-    ax2 = fig.add_subplot(2, 2, 2)
-    p2 = sns.scatterplot(x="Ligand Distortion Energy (kcal/mol)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand", legend=False, s=100)
-    for line in range(1, df.shape[0] + 1):
-        ax2.text(df["Ligand Distortion Energy (kcal/mol)"][line]+5, df["Addition Barrier (kcal/mol)"][line],
-                df["Ligand"][line], horizontalalignment='left', size='small', color='black')
-    ax3 = fig.add_subplot(2, 2, 3)
-    p3 = sns.scatterplot(x="Activation Energy (kcal/mol)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand", legend=False, s=100)
-    for line in range(1, df.shape[0] + 1):
-        ax3.text(df["Activation Energy (kcal/mol)"][line]-0.5, df["Addition Barrier (kcal/mol)"][line],
-                df["Ligand"][line], horizontalalignment='right', size='small', color='black')
-    ax4 = fig.add_subplot(2, 2, 4)
-    p4 = sns.scatterplot(x="Interaction Energy (kcal/mol)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand", legend=False, s=100)
-    for line in range(1, df.shape[0] + 1):
-        ax4.text(df["Interaction Energy (kcal/mol)"][line]+5, df["Addition Barrier (kcal/mol)"][line],
-                df["Ligand"][line], horizontalalignment='left', size='small', color='black')
-    # plt.legend(p4, loc="center right", borderaxespad=0.1, title="Ligands")
-    plt.ylim(None, None); plt.xlim(None, None)
-    # plt.tight_layout()
-    plt.savefig("Distortion-Interaction Analysis")
-    # plt.show()
-
+    # Base for elimination
+    for i in [1, 2]:
+        # Update DATA_PATH and reinitialise residue_dict for every replicate
+        DATA_DIR = "{0}/{1}/MD/cov/Rep{2}/analysis/base".format(MD_PATH, "3", i)
+        residue_dict = {"arg": {"filename_list": [], },
+                        "glu": {"filename_list": [], },
+                        "asp": {"filename_list": [], },
+                        "his": {"filename_list": [], },
+                        "lys": {"filename_list": [], }}
+        # Sort out all .dat files
+        for j, filename in enumerate(os.listdir(DATA_DIR)):
+            # Filter out files that are not .dat files
+            if ".dat" not in filename:
+                continue
+            # assign all .dat files to respective residues dictionary
+            for k, residue in enumerate(residue_dict.keys()):
+                if residue in filename:
+                    residue_dict[residue]["filename_list"].append(filename)
+        # Analyse each residue
+        for j, residue in enumerate(residue_dict.keys()):
+            combined_df = pd.DataFrame(list(range(0, 20001, 1)), columns=["Time (ps)"])  # Create empty dataframe
+            for k, dat_file in enumerate(residue_dict[residue]["filename_list"]):
+                resname = dat_file.split(".")[0].split("_")[-1].upper()
+                df = pd.read_csv("{0}/{1}".format(DATA_DIR, dat_file), index_col=0, delim_whitespace=True)
+                df.columns = [resname]
+                combined_df = pd.concat([combined_df, df], axis=1, ignore_index=False)
+            combined_df = combined_df.drop([0], axis=0)
+            combined_df = combined_df.melt("Time (ps)", var_name="Residues", value_name=r"Distance ($\AA$)")
+            fig = plt.figure(figsize=(SINGLE_PLOT_WIDTH, SINGLE_PLOT_HEIGHT))
+            fig.subplots_adjust(top=0.9, bottom=0.1, left=0.1, right=0.9)
+            ax = sns.lineplot(x="Time (ps)", y=r"Distance ($\AA$)", data=combined_df.head(100000), hue="Residues")
+            leg = ax.legend(ncol=3, loc='best', fontsize='small')
+            leg.set_title("")
+            plt.ylim(None, None); plt.xlim(None, None)
+            plt.tight_layout()
+            plt.show()
+            plt.savefig(r"Distance of C$_\alpha$ H from {0} Residues Replicate {1}".format(residue.upper(), i))
 
