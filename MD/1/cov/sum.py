1122c1f6662b3f6bec5255881715c61c06e55768
Jonathan Ting <yikchangjonathan.ting@uq.edu.au>
2019-09-12 11:16:33 +1000

    Reparametrised and rearranged AR8U.mtb (Fix wrong charge on S)

---
+++ MD/1/cov/sum.py
@@ -0,0 +1,15 @@
+total,modify = 0, 0.00
+with open('charges.txt', 'r') as inp, open('tot_charge.txt', 'w') as outp:
+   for line in inp:
+       try:
+           num = float(line) + modify
+           total += num
+           if num >= 0:
+               outp.write(" {:3f}\n".format(num))
+           else:
+               outp.write("{:3f}\n".format(num))
+       except ValueError:
+           print('{} is not a number!'.format(line))
+total -= modify * 6
+print('Total charges: {0:1f}'.format(total))
+
