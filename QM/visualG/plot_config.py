04e98ae50a8b7265df8f531fc5f7335d900c9f68
Jon Ting <jonting97@gmail.com>
2019-09-17 11:20:26 +1000

    Before implementing plotting using loops

--- QM/visualG/plot_config.py
+++ QM/visualG/plot_config.py
@@ -7,6 +7,8 @@ Though recognition of my effort is appreciated! XD
 """
 
 
+charge_list = ["Mulliken", "NBO", "MK", "Hirshfeld", "CM5", "AIM", "ChelpG", "Omega"]
+
 combination_dict = {
     "I": {
         "LUMO": {
@@ -16,7 +18,9 @@ combination_dict = {
             "X-AXIS": 0,
             "Y-AXIS": 0.4,
             "LEG": "upper left",
-            "FONTSIZE": "medium"
+            "ALIGN": "center",
+            "FONTSIZE": "medium",
+            "X-NAME": "Ligand LUMO Energy (kcal/mol)"
         },
 
         "Charge": {
@@ -27,108 +31,132 @@ combination_dict = {
                 "X-AXIS": -0.03,
                 "Y-AXIS": 0,
                 "LEG": "upper right",
-                "FONTSIZE": "x-small"
+                "ALIGN": "right",
+                "FONTSIZE": "x-small",
+                "X-NAME": "Mulliken Charge (e)"
             },
             "NBO": {
-                "M": -3.5523,
-                "C": 10.639,
-                "R2": 0.2152,
-                "X-AXIS": -0.03,
+                "M": -24.802,
+                "C": 9.7637,
+                "R2": 0.7967,
+                "X-AXIS": -0.01,
                 "Y-AXIS": 0,
                 "LEG": "upper right",
-                "FONTSIZE": "x-small"
+                "ALIGN": "right",
+                "FONTSIZE": "x-small",
+                "X-NAME": "NBO Charge (e)"
             },
             "MK": {
-                "M": -3.5523,
-                "C": 10.639,
-                "R2": 0.2152,
-                "X-AXIS": -0.03,
+                "M": -7.1748,
+                "C": 11.747,
+                "R2": 0.3866,
+                "X-AXIS": 0.02,
                 "Y-AXIS": 0,
                 "LEG": "upper right",
-                "FONTSIZE": "x-small"
+                "ALIGN": "left",
+                "FONTSIZE": "x-small",
+                "X-NAME": "Merz-Kollman Charge (e)"
             },
             "Hirshfeld": {
-                "M": -3.5523,
-                "C": 10.639,
-                "R2": 0.2152,
-                "X-AXIS": -0.03,
+                "M": -89.348,
+                "C": 12.659,
+                "R2": 0.8491,
+                "X-AXIS": 0.005,
                 "Y-AXIS": 0,
                 "LEG": "upper right",
-                "FONTSIZE": "x-small"
+                "ALIGN": "left",
+                "FONTSIZE": "x-small",
+                "X-NAME": "Hirshfeld Charge (e)"
             },
             "CM5": {
-                "M": -3.5523,
-                "C": 10.639,
-                "R2": 0.2152,
-                "X-AXIS": -0.03,
+                "M": -54.454,
+                "C": 8.8508,
+                "R2": 0.7741,
+                "X-AXIS": -0.005,
                 "Y-AXIS": 0,
-                "LEG": "upper right",
-                "FONTSIZE": "x-small"
-            },
-            "ChelpG": {
-                "M": -3.5523,
-                "C": 10.639,
-                "R2": 0.2152,
-                "X-AXIS": -0.03,
-                "Y-AXIS": 0,
-                "LEG": "upper right",
-                "FONTSIZE": "x-small"
+                "LEG": "lower left",
+                "ALIGN": "right",
+                "FONTSIZE": "x-small",
+                "X-NAME": "CM5 Charge (e)"
             },
             "AIM": {
-                "M": -3.5523,
-                "C": 10.639,
-                "R2": 0.2152,
-                "X-AXIS": -0.03,
+                "M": -92.849,
+                "C": 14.411,
+                "R2": 0.8573,
+                "X-AXIS": -0.003,
                 "Y-AXIS": 0,
                 "LEG": "upper right",
-                "FONTSIZE": "x-small"
+                "ALIGN": "right",
+                "FONTSIZE": "x-small",
+                "X-NAME": "AIM Charge (e)"
+            },
+            "ChelpG": {
+                "M": -19.394,
+                "C": 9.9205,
+                "R2": 0.6864,
+                "X-AXIS": -0.004,
+                "Y-AXIS": 0.2,
+                "LEG": "upper right",
+                "ALIGN": "right",
+                "FONTSIZE": "x-small",
+                "X-NAME": "ChelpG Charge (e)"
             },
             "Omega": {
-                "M": -3.5523,
-                "C": 10.639,
-                "R2": 0.2152,
-                "X-AXIS": -0.03,
+                "M": -0.7225,
+                "C": 56.999,
+                "R2": 0.9376,
+                "X-AXIS": -0.4,
                 "Y-AXIS": 0,
                 "LEG": "upper right",
-                "FONTSIZE": "x-small"
+                "ALIGN": "right",
+                "FONTSIZE": "x-small",
+                "X-NAME": "Electrophilicity Index"
             },
         },
         "DI": {
             "Thiolate": {
-                "M": -3.5523,
-                "C": 10.639,
-                "R2": 0.2152,
-                "X-AXIS": -0.03,
+                "M": 17.241,
+                "C": 7.6018,
+                "R2": 0.798,
+                "X-AXIS": -0.02,
                 "Y-AXIS": 0,
-                "LEG": "upper right",
-                "FONTSIZE": "x-small"
+                "LEG": "upper left",
+                "ALIGN": "right",
+                "FONTSIZE": "x-small",
+                "X-NAME": "Thiolate Distortion Energy (kcal/mol)"
             },
             "Ligand": {
-                "M": -3.5523,
-                "C": 10.639,
-                "R2": 0.2152,
-                "X-AXIS": -0.03,
-                "Y-AXIS": 0,
-                "LEG": "upper right",
-                "FONTSIZE": "x-small"
+                "M": 0.8082,
+                "C": 5.3355,
+                "R2": 0.9760,
+                "X-AXIS": -1.0,
+                "Y-AXIS": 0.7,
+                "LEG": "best",
+                "ALIGN": "left",
+                "FONTSIZE": "x-small",
+                "X-NAME": "Ligand Distortion Energy (kcal/mol)"
             },
             "Activation": {
-                "M": -3.5523,
-                "C": 10.639,
-                "R2": 0.2152,
-                "X-AXIS": -0.03,
+                "M": 0.9013,
+                "C": 10.439,
+                "R2": 0.8943,
+                "X-AXIS": -0.3,
                 "Y-AXIS": 0,
-                "LEG": "upper right",
-                "FONTSIZE": "x-small"
+                "LEG": "upper left",
+                "ALIGN": "right",
+                "FONTSIZE": "x-small",
+                "X-NAME": "Activation Energy (kcal/mol)"
             },
             "Interaction": {
-                "M": -3.5523,
-                "C": 10.639,
-                "R2": 0.2152,
-                "X-AXIS": -0.03,
+                "M": -1.6397,
+                "C": 0.1818,
+                "R2": 0.428,
+                "X-AXIS": 0.18,
                 "Y-AXIS": 0,
                 "LEG": "upper right",
-                "FONTSIZE": "x-small"
+                "ALIGN": "left",
+                "FONTSIZE": "x-small",
+                "X-NAME": "Interaction Energy (kcal/mol)"
             },
         }
     },
--- QM/visualG/plot_fig.py
+++ QM/visualG/plot_fig.py
@@ -11,7 +11,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from QM.run_gaussian.settings import DATA_PATH
-from QM.visualG.plot_config import combination_dict
+from QM.visualG.plot_config import combination_dict, charge_list
 
 sns.set(context='paper', font_scale=1.5)
 # sns.despine()  # Remove "chartjunk"
@@ -36,100 +36,129 @@ if __name__ == "__main__":
     print(df)
     prop_dict = combination_dict[combination[-1]]
 
-
     # Ligand LUMO Energy
     # sorted_df = df.sort_values(by=["Ligand LUMO Energy (kcal/mol)"])
     chosen_dict = prop_dict["LUMO"]
-    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict["X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     fig = plt.figure(figsize=(SINGLE_PLOT_WIDTH, SINGLE_PLOT_HEIGHT))
     fig.subplots_adjust(top=0.9, bottom=0.1, left=0.1, right=0.95)
     ax = sns.scatterplot(x="Ligand LUMO Energy (kcal/mol)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand", legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
         ax.text(df["Ligand LUMO Energy (kcal/mol)"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                 df["Ligand"][line], horizontalalignment='center', size='small', color='black')
-    # m, c, r2 = 0.6121, 22.143, 0.8354
     lin_reg(m, c, r2, plt.xlim(), leg, fontsize)
     plt.ylim(None, None); plt.xlim(None, None)
     plt.tight_layout()
     plt.savefig("{0}/Ligand LUMO Energy".format(combination))
-
 
     # Beta-Carbon Charges
     fig = plt.figure(figsize=(EIGHT_PLOTS_WIDTH, EIGHT_PLOTS_HEIGHT))
     fig.subplots_adjust(top=0.9, bottom=0.1, left=0.1, right=0.95, wspace=0.3, hspace=0.45)
     fig.suptitle(r"Correlation between $\beta$-Carbon Charge and Addition Barrier",
                  horizontalalignment='center', fontsize=16, weight='bold')
+
+    for i, model in enumerate(charge_list):
+        chosen_dict = prop_dict["Charge"][model]
+        m, c, r2, x_axis, y_axis, leg, txt_align, fontsize, x_name = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+            "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["ALIGN"], chosen_dict["FONTSIZE"], chosen_dict["X-NAME"]
+        ax1 = fig.add_subplot(4, 2, i + 1)
+        p1 = sns.scatterplot(x=x_name, y="Addition Barrier (kcal/mol)", data=df,
+                             hue="Ligand", legend=False, s=100)
+        for line in range(1, df.shape[0] + 1):
+            ax1.text(df[x_name][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
+                     df["Ligand"][line], horizontalalignment=txt_align, size='small', color='black')
+        lin_reg(m, c, r2, plt.xlim(), leg, fontsize)
+    plt.savefig("{0}/Beta-Carbon Charges".format(combination))
+
+    raise
+    chosen_dict = prop_dict["Charge"]["Mulliken"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     ax1 = fig.add_subplot(4, 2, 1)
     p1 = sns.scatterplot(x="Mulliken Charge (e)", y="Addition Barrier (kcal/mol)", data=df,
                          hue="Ligand", legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
-        ax1.text(df["Mulliken Charge (e)"][line]-0.03, df["Addition Barrier (kcal/mol)"][line],
+        ax1.text(df["Mulliken Charge (e)"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                  df["Ligand"][line], horizontalalignment='right', size='small', color='black')
-    m, c, r2 = -3.5523, 10.639, 0.2152
-    lin_reg(m, c, r2, plt.xlim(), "upper right")
+    lin_reg(m, c, r2, plt.xlim(), leg, fontsize)
 
+    chosen_dict = prop_dict["Charge"]["NBO"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     ax2 = fig.add_subplot(4, 2, 2)
     p2 = sns.scatterplot(x="NBO Charge (e)", y="Addition Barrier (kcal/mol)", data=df,
                          hue="Ligand", legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
-        ax2.text(df["NBO Charge (e)"][line]-0.01, df["Addition Barrier (kcal/mol)"][line],
+        ax2.text(df["NBO Charge (e)"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                  df["Ligand"][line], horizontalalignment='right', size='small', color='black')
-    m, c, r2 = -24.802, 9.7637, 0.7967
-    lin_reg(m, c, r2, plt.xlim(), "upper right")
+    lin_reg(m, c, r2, plt.xlim(), leg, fontsize)
 
+    chosen_dict = prop_dict["Charge"]["MK"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     ax3 = fig.add_subplot(4, 2, 3)
     p3 = sns.scatterplot(x="Merz-Kollman Charge (e)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand",
                          legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
-        ax3.text(df["Merz-Kollman Charge (e)"][line]+0.02, df["Addition Barrier (kcal/mol)"][line],
+        ax3.text(df["Merz-Kollman Charge (e)"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                  df["Ligand"][line], horizontalalignment='left', size='small', color='black')
-    m, c, r2 = -7.1748, 11.747, 0.3866
-    lin_reg(m, c, r2, plt.xlim(), "upper right")
+    lin_reg(m, c, r2, plt.xlim(), leg, fontsize)
 
+    chosen_dict = prop_dict["Charge"]["Hirshfeld"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     ax4 = fig.add_subplot(4, 2, 4)
     p4 = sns.scatterplot(x="Hirshfeld Charge (e)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand",
                          legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
-        ax4.text(df["Hirshfeld Charge (e)"][line]+0.005, df["Addition Barrier (kcal/mol)"][line],
+        ax4.text(df["Hirshfeld Charge (e)"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                  df["Ligand"][line], horizontalalignment='left', size='small', color='black')
-    m, c, r2 = -89.348, 12.659, 0.8491
-    lin_reg(m, c, r2, plt.xlim(), "upper right")
+    lin_reg(m, c, r2, plt.xlim(), leg, fontsize)
 
+    chosen_dict = prop_dict["Charge"]["CM5"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     ax5 = fig.add_subplot(4, 2, 5)
     p5 = sns.scatterplot(x="CM5 Charge (e)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand",
                          legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
-        ax5.text(df["CM5 Charge (e)"][line]-0.005, df["Addition Barrier (kcal/mol)"][line],
+        ax5.text(df["CM5 Charge (e)"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                  df["Ligand"][line], horizontalalignment='right', size='small', color='black')
-    m, c, r2 = -54.454, 8.8508, 0.7741
-    lin_reg(m, c, r2, plt.xlim(), "lower left")
+    lin_reg(m, c, r2, plt.xlim(), leg, fontsize)
 
+    chosen_dict = prop_dict["Charge"]["AIM"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     ax6 = fig.add_subplot(4, 2, 6)
     p6 = sns.scatterplot(x="AIM Charge (e)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand",
                          legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
-        ax6.text(df["AIM Charge (e)"][line]-0.003, df["Addition Barrier (kcal/mol)"][line],
+        ax6.text(df["AIM Charge (e)"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                  df["Ligand"][line], horizontalalignment='right', size='small', color='black')
-    m, c, r2 = -92.849, 14.411, 0.8573
-    lin_reg(m, c, r2, plt.xlim(), "upper right")
+    lin_reg(m, c, r2, plt.xlim(), leg, fontsize)
 
+    chosen_dict = prop_dict["Charge"]["ChelpG"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     ax7 = fig.add_subplot(4, 2, 7)
     p5 = sns.scatterplot(x="ChelpG Charge (e)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand",
                          legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
-        ax7.text(df["ChelpG Charge (e)"][line]-0.004, df["Addition Barrier (kcal/mol)"][line]+0.2,
+        ax7.text(df["ChelpG Charge (e)"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                  df["Ligand"][line], horizontalalignment='right', size='small', color='black')
-    m, c, r2 = -19.394, 9.9205, 0.6864
-    lin_reg(m, c, r2, plt.xlim(), "upper right")
+    lin_reg(m, c, r2, plt.xlim(), leg, fontsize)
 
+    chosen_dict = prop_dict["Charge"]["Omega"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     ax8 = fig.add_subplot(4, 2, 8)
     p6 = sns.scatterplot(x="Electrophilicity Index", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand",
                          legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
-        ax8.text(df["Electrophilicity Index"][line]-0.4, df["Addition Barrier (kcal/mol)"][line],
+        ax8.text(df["Electrophilicity Index"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                  df["Ligand"][line], horizontalalignment='right', size='small', color='black')
-    m, c, r2 = -0.7225, 56.999, 0.9376
-    lin_reg(m, c, r2, plt.xlim(), "upper right")
+    lin_reg(m, c, r2, plt.xlim(), leg, fontsize)
 
     # plt.legend(p4, loc="center right", borderaxespad=0.1, title="Ligands")
     # plt.ylim(3, 15); plt.xlim(None, None)
@@ -137,42 +166,54 @@ if __name__ == "__main__":
     plt.savefig("{0}/Beta-Carbon Charges".format(combination))
     # plt.show()
 
-
     # Distortion/Interaction Analysis
     fig = plt.figure(figsize=(FOUR_PLOTS_WIDTH, FOUR_PLOTS_HEIGHT))
     # fig, axes = plt.subplots(nrows=2, ncols=2, sharex=False, sharey=True)
     fig.subplots_adjust(top=0.9, bottom=0.1, left=0.1, right=0.95, wspace=0.3, hspace=0.45)
     fig.suptitle("Correlation between Distortion/Interaction Energies and Addition Barrier", horizontalalignment='center', fontsize=16, weight='bold')
+
+    chosen_dict = prop_dict["DI"]["Thiolate"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     ax1 = fig.add_subplot(2, 2, 1)
     p1 = sns.scatterplot(x="Thiolate Distortion Energy (kcal/mol)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand", legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
-        ax1.text(df["Thiolate Distortion Energy (kcal/mol)"][line]-0.02, df["Addition Barrier (kcal/mol)"][line],
+        ax1.text(df["Thiolate Distortion Energy (kcal/mol)"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                 df["Ligand"][line], horizontalalignment='right', size='small', color='black')
-    m, c, r2 = 17.241, 7.6018, 0.798
-    lin_reg(m, c, r2, plt.xlim(), "upper left")
+    # m, c, r2 = 17.241, 7.6018, 0.798
+    lin_reg(m, c, r2, plt.xlim(), leg, fontsize)
 
+    chosen_dict = prop_dict["DI"]["Ligand"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     ax2 = fig.add_subplot(2, 2, 2)
     p2 = sns.scatterplot(x="Ligand Distortion Energy (kcal/mol)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand", legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
-        ax2.text(df["Ligand Distortion Energy (kcal/mol)"][line]-1.0, df["Addition Barrier (kcal/mol)"][line]+0.7,
+        ax2.text(df["Ligand Distortion Energy (kcal/mol)"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                 df["Ligand"][line], horizontalalignment='left', size='small', color='black')
-    m, c, r2 = 0.8082, 5.3355, 0.9760
+    # m, c, r2 = 0.8082, 5.3355, 0.9760
     lin_reg(m, c, r2, plt.xlim(), "best")
 
+    chosen_dict = prop_dict["DI"]["Activation"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     ax3 = fig.add_subplot(2, 2, 3)
     p3 = sns.scatterplot(x="Activation Energy (kcal/mol)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand", legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
-        ax3.text(df["Activation Energy (kcal/mol)"][line]-0.3, df["Addition Barrier (kcal/mol)"][line],
+        ax3.text(df["Activation Energy (kcal/mol)"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                 df["Ligand"][line], horizontalalignment='right', size='small', color='black')
-    m, c, r2 = 0.9013, 10.439, 0.8943
+    # m, c, r2 = 0.9013, 10.439, 0.8943
     lin_reg(m, c, r2, plt.xlim(), "upper left")
 
+    chosen_dict = prop_dict["DI"]["Interaction"]
+    m, c, r2, x_axis, y_axis, leg, fontsize = chosen_dict["M"], chosen_dict["C"], chosen_dict["R2"], chosen_dict[
+        "X-AXIS"], chosen_dict["Y-AXIS"], chosen_dict["LEG"], chosen_dict["FONTSIZE"]
     ax4 = fig.add_subplot(2, 2, 4)
     p4 = sns.scatterplot(x="Interaction Energy (kcal/mol)", y="Addition Barrier (kcal/mol)", data=df, hue="Ligand", legend=False, s=100)
     for line in range(1, df.shape[0] + 1):
-        ax4.text(df["Interaction Energy (kcal/mol)"][line]+0.18, df["Addition Barrier (kcal/mol)"][line],
+        ax4.text(df["Interaction Energy (kcal/mol)"][line] + x_axis, df["Addition Barrier (kcal/mol)"][line] + y_axis,
                 df["Ligand"][line], horizontalalignment='left', size='small', color='black')
-    m, c, r2 = -1.6397, 0.1818, 0.428
+    # m, c, r2 = -1.6397, 0.1818, 0.428
     lin_reg(m, c, r2, plt.xlim(), "lower left")
 
     # plt.legend(p4, loc="center right", borderaxespad=0.1, title="Ligands")
