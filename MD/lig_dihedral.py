28bec6b34638e4a7142ec0f7ed02c88a9ac2bf83
Jonathan Ting <yikchangjonathan.ting@uq.edu.au>
2019-10-18 17:03:29 +1000

    Change MD plots to 3 columns

--- MD/lig_dihedral.py
+++ MD/lig_dihedral.py
@@ -12,7 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from Honours.MD.config import MD_PATH, SIX_PLOTS_WIDTH, SIX_PLOTS_HEIGHT, inhibitor_list
+from Honours.MD.config import MD_PATH, SIX_PLOTS_WIDTH, SIX_PLOTS_HEIGHT, SIX_PLOTS_HORIZONTAL_WIDTH, SIX_PLOTS_HORIZONTAL_HEIGHT, inhibitor_list
 
 sns.set(context='paper', font_scale=1.5)
 CURR_DIR = os.getcwd()
@@ -22,18 +22,20 @@ if __name__ == "__main__":
 
     system_type, num_subplots, fig_type = "noncov", 6, "Hist"
 
-    PLOTS_WIDTH, PLOTS_HEIGHT = SIX_PLOTS_WIDTH, SIX_PLOTS_HEIGHT
-    num_row, subplot_index = int(num_subplots / 2), 0
-    fig, axes = plt.subplots(nrows=num_row, ncols=2, sharex=True, sharey=True, figsize=(PLOTS_WIDTH, PLOTS_HEIGHT))
+    # PLOTS_WIDTH, PLOTS_HEIGHT = SIX_PLOTS_WIDTH, SIX_PLOTS_HEIGHT
+    PLOTS_WIDTH, PLOTS_HEIGHT = SIX_PLOTS_HORIZONTAL_WIDTH, SIX_PLOTS_HORIZONTAL_HEIGHT
+    num_row, subplot_index = int(num_subplots / 3), 0
+    fig, axes = plt.subplots(nrows=num_row, ncols=3, sharex=True, sharey=True, figsize=(PLOTS_WIDTH, PLOTS_HEIGHT))
     if fig_type == "Line":
         fig.subplots_adjust(top=0.95, bottom=0.06, left=0.10, right=0.97, wspace=0.25, hspace=0.15)
         fig.text(0.5, 0.02, "Time (ns)", va='center', ha='center')
         fig.text(0.02, 0.5, u"Angle (\N{DEGREE SIGN})", va='center', ha='center', rotation='vertical')
     elif fig_type == "Hist":
-        fig.subplots_adjust(top=0.95, bottom=0.06, left=0.10, right=0.97, wspace=0.25, hspace=0.15)
+        #fig.subplots_adjust(top=0.95, bottom=0.06, left=0.10, right=0.97, wspace=0.25, hspace=0.15)
+        fig.subplots_adjust(top=0.98, bottom=0.08, left=0.09, right=0.98, wspace=0.25, hspace=0.15)
         fig.text(0.5, 0.02, u"Angle (\N{DEGREE SIGN})", va='center', ha='center')
         fig.text(0.02, 0.5, "Population", va='center', ha='center', rotation='vertical')
-    fig.suptitle("Dihedral of C=C-C=O For All Inhibitors", horizontalalignment='center', fontsize=14, weight='bold')
+    # fig.suptitle("Dihedral of C=C-C=O For All Inhibitors", horizontalalignment='center', fontsize=14, weight='bold')
 
     for i, inhibitor in enumerate(inhibitor_list):
         combined_df = pd.DataFrame(list(np.arange(0.005, 100.005, 0.005)), columns=["Time (ns)"])
@@ -51,7 +53,7 @@ if __name__ == "__main__":
         col_label += label_list
         combined_df.columns = col_label
         subplot_index += 1
-        ax1 = plt.subplot(int("{0}2{1}".format(num_row, subplot_index)))
+        ax1 = plt.subplot(int("{0}3{1}".format(num_row, subplot_index)))
         if fig_type == "Line":
             combined_df.plot(x="Time (ns)", y=label_list, kind="line", ax=plt.gca(), lw=0.3, linestyle=":")
             ax1.xaxis.label.set_visible(False)
@@ -62,8 +64,10 @@ if __name__ == "__main__":
         leg = plt.legend(loc="upper right")
         plt.setp(leg.get_lines(), linewidth=4)
         # plt.locator_params(axis='y', nbins=6)
-    fig.text(0.1, 0.941, "(a)", va='center', ha='left'); fig.text(0.583, 0.941, "(b)", va='center', ha='left')
-    fig.text(0.1, 0.6315, "(c)", va='center', ha='left'); fig.text(0.583, 0.6315, "(d)", va='center', ha='left')
-    fig.text(0.1, 0.32, "(e)", va='center', ha='left'); fig.text(0.583, 0.32, "(f)", va='center', ha='left')
-    plt.savefig("{0}/Dihedral of C=C-C=O For All Inhibitors {1}".format(CURR_DIR, fig_type))
+    # fig.text(0.1, 0.941, "(a)", va='center', ha='left'); fig.text(0.583, 0.941, "(b)", va='center', ha='left')
+    # fig.text(0.1, 0.6315, "(c)", va='center', ha='left'); fig.text(0.583, 0.6315, "(d)", va='center', ha='left')
+    # fig.text(0.1, 0.32, "(e)", va='center', ha='left'); fig.text(0.583, 0.32, "(f)", va='center', ha='left')
+    fig.text(0.089, 0.965, "(a)", va='center', ha='left'); fig.text(0.407, 0.965, "(b)", va='center', ha='left'), fig.text(0.725, 0.965, "(c)", va='center', ha='left')
+    fig.text(0.089, 0.483, "(d)", va='center', ha='left'); fig.text(0.407, 0.483, "(e)", va='center', ha='left'); fig.text(0.725, 0.483, "(f)", va='center', ha='left')
+    plt.savefig("{0}/Dihedral of C=C-C=O For All Inhibitors {1} Test".format(CURR_DIR, fig_type))
     plt.show()
